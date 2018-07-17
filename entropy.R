eac3ad66e3bf3e60d0f9285f3cea893bc7b962c9
André Vidas Olsen <andre.v.olsen@gmail.com>
2018-07-17 15:57:12 +0200

    Example data with normalization of entropy. Can be used in the CERES project

--- entropy.R
+++ entropy.R
@@ -23,4 +23,9 @@ entropy(c(10,10000,10,1000,100,10,1))
 entropy(c(1,10,3400,5))
 
 # average entropy (maybe for CERES project)
-(entropy(c(1,3)) + entropy(c(3,1)) + entropy(c(4,0)) + entropy(c(2,2)) + entropy(c(2,2)) + entropy(c(2,2)))/6
+(entropy(c(4,0)) + entropy(c(0,4)) + entropy(c(4,0)) + entropy(c(0,4)) + entropy(c(0,4)) + entropy(c(4,0)))/6
+
+(entropy(c(3,1,3,4)) + entropy(c(2,2,1,1)) + entropy(c(4,0,6,5)) + entropy(c(2,2,5,4)) + entropy(c(1,3,2,76)) + entropy(c(2,2,2,3)))/(6*log2(4)) # normalized entropy after average ( divided by log2(length(S)) )
+
+
+(entropy(c(1,1,1,1)) + entropy(c(1,1,1,1)))/(2*log2(4))
