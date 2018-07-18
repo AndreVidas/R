277735c45683bfed9eb266d24d984ec31aaa59d8
André Vidas Olsen <andre.v.olsen@gmail.com>
2018-07-18 10:04:47 +0200

    Implement normalization option

--- entropy.R
+++ entropy.R
@@ -1,13 +1,17 @@
 rm(list=ls())
-entropy <- function(S){
+entropy <- function(S, normalize = FALSE){
     #
     # This function calculates entropy
     #
 
-    sumOfS <- sum(S)
+    sumOfS <- sum(as.numeric(S))
     H <- ifelse(S == 0, 0, (S/sumOfS)*log2(S/sumOfS))  
     H_S <- -sum(H)
-
+    
+    
+    if(normalize){
+        H_S <- H_S/log2(length(S))
+    }
     
     
     return(H_S)
@@ -22,6 +26,10 @@ entropy(c(1,10,3400,5))
 # average entropy (maybe for CERES project)
 (entropy(c(4,0)) + entropy(c(0,4)) + entropy(c(4,0)) + entropy(c(0,4)) + entropy(c(0,4)) + entropy(c(4,0)))/6
 (entropy(c(3,1,3,4)) + entropy(c(2,2,1,1)) + entropy(c(4,0,6,5)) + entropy(c(2,2,5,4)) + entropy(c(1,3,2,76)) + entropy(c(2,2,2,3)))/(6*log2(4)) # normalized entropy after average ( divided by log2(length(S)) )
+(entropy(c(3,1,3,4), normalize = TRUE) + entropy(c(2,2,1,1), normalize = TRUE) + entropy(c(4,0,6,5), normalize = TRUE) + entropy(c(2,2,5,4), normalize = TRUE) + entropy(c(1,3,2,76), normalize = TRUE) + entropy(c(2,2,2,3), normalize = TRUE))/6 # test normalization option gives the same results as the above manual normalization calculation
 (entropy(c(1,1,1,1)) + entropy(c(1,1,1,1)))/(2*log2(4))
 
+
+
+entropy(sample(10))/log2(10)
 
