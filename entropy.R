aabb29b1552c82ade5479ecb7e2192ebf24b68f4
André Vidas Olsen <andre.v.olsen@gmail.com>
2018-07-18 10:18:13 +0200

    Implement roxygen2 documentation

--- entropy.R
+++ entropy.R
@@ -1,14 +1,18 @@
 rm(list=ls())
-entropy <- function(S, normalize = FALSE){
-    #
-    # This function calculates entropy
-    #
 
+
+#' @author André Vidas Olsen
+#' @description This function calculates Shannon's entropy
+#' @param S a vector containing observations for each attribute
+#' @param normalize boolean option for normalizing entropy
+entropy <- function(S, normalize = FALSE){
+
+    # calculate entropy
     sumOfS <- sum(as.numeric(S))
     H <- ifelse(S == 0, 0, (S/sumOfS)*log2(S/sumOfS))  
     H_S <- -sum(H)
     
-    
+    # normalize entropy option
     if(normalize){
         H_S <- H_S/log2(length(S))
     }
