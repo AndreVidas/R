1d707fa5992bb4fd3ad4c28af6d64463945ee95c
André Vidas Olsen <andre.v.olsen@gmail.com>
2018-07-23 13:06:28 +0200

    small change

--- monteCarloEstimationOfPi.R
+++ monteCarloEstimationOfPi.R
@@ -1,5 +1,5 @@
 ####################################################################################
-# monte carlo simulation
+# monte carlo estimation of Pi
 ####################################################################################
 
 # generate random points between (0,0) and (1,1)
@@ -11,9 +11,11 @@ points <- data.frame(x = sample(nrOfPoints), y = sample(nrOfPoints))/nrOfPoints
 # points outside a circle with radius 1 is labelled as "outside"
 points$thresh <- ifelse(sqrt(points$x**2 + points$y**2) <= 1, "within", "outside")
 
-# estimation of pi
+# estimate pi
 4*(nrow(points[points$thresh == "within",])/nrow(points))
 
 # plot points with colored labels
 plot(points[points$thresh == "within","x"], points[points$thresh == "within","y"], col = "red")
 points(points[points$thresh == "outside","x"], points[points$thresh == "outside","y"], col = "blue")
+
+
